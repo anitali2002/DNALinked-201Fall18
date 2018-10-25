b0db82e80d6c1e1d830f0a9163b158f4da0f368c
Anita Li <anita.li@duke.edu>
2018-10-25 11:40:22 -0400

    update?

--- src/LinkStrand.java
+++ src/LinkStrand.java
@@ -148,7 +148,7 @@ public class LinkStrand implements IDnaStrand {
 //			StringBuilder forNode = new StringBuilder(copy.info);
 //			String reversed = forNode.reverse().toString();
 //			reverse.myFirst = new Node(reversed);
-			reverse.myFirst = new Node(copy.info);
+//			reverse.myFirst = new Node(copy.info);
 			reverse.myFirst = new Node(copy.info, reverse.myFirst);
 			StringBuilder forNode = new StringBuilder(copy.next.info);
 			forNode.reverse();
